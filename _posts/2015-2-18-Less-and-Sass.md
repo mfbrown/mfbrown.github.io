51e98ce91e70a299fadd101d0e0a79c4e1cec6e7
Michael Brown <michaelfbrown.88@gmail.com>
2015-03-03 07:26:21 -0500

    start SMACSS organization

---
+++ _posts/2015-2-18-Less-and-Sass.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Understanding the Differences Between Less and Sass"
+date:   2015-2-18
+categories: Less, Sass, Scss, CSS
+---
+
+##
--- _site/gulpfile.js
+++ _site/gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*', 'styleguide/*.html'], ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_layouts/*.html', '_includes/*.html', '_posts/*', 'styleguide/*.html'], ['jekyll-rebuild']);
 });
 
 // Deploy Task
